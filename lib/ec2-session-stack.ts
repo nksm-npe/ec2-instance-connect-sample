305b1025e6e166ac15eabb12d71563c6dea89c23
nksm-npe <notpoorelephant@gmail.com>
2024-08-18 12:15:51 +0900

    ongoing

--- lib/ec2-session-stack.ts
+++ lib/ec2-session-stack.ts
@@ -102,7 +102,7 @@ export class Ec2SessionStack extends Stack {
     //   "curl https://amazon-ec2-instance-connect-us-west-2.s3.us-west-2.amazonaws.com/latest/linux_amd64/ec2-instance-connect.rhel8.rpm -o /tmp/ec2-instance-connect/ec2-instance-connect.rpm",
     //   "curl https://amazon-ec2-instance-connect-us-west-2.s3.us-west-2.amazonaws.com/latest/linux_amd64/ec2-instance-connect-selinux.noarch.rpm -o /tmp/ec2-instance-connect/ec2-instance-connect-selinux.rpm",
     //   "sudo yum install -y /tmp/ec2-instance-connect/ec2-instance-connect.rpm /tmp/ec2-instance-connect/ec2-instance-connect-selinux.rpm"
-    // );
+    // ); // CentOS Stream8用。yum を通すために、rpm設定を編集しないといけない
     userData.addCommands(
       "mkdir /tmp/ec2-instance-connect",
       "curl https://amazon-ec2-instance-connect-us-west-2.s3.us-west-2.amazonaws.com/latest/linux_amd64/ec2-instance-connect.rpm -o /tmp/ec2-instance-connect/ec2-instance-connect.rpm",
