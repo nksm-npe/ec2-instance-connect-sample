320e09e644124238d03a78ad3ed614b1eb74e7df
nksm-npe <notpoorelephant@gmail.com>
2024-09-01 14:08:50 +0900

    ongoing

--- bin/ec2-session.ts
+++ bin/ec2-session.ts
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
-// import { Ec2SessionStack } from "../lib/ec2-session-stack";
+import { Ec2SessionStack } from "../lib/ec2-session-stack";
 import { IpV6Ec2SessionStack } from "../lib/ipv6-ec2-session-stack";
 
 const app = new cdk.App();
-// new Ec2SessionStack(app, "Ec2SessionStack");
+new Ec2SessionStack(app, "Ec2SessionStack");
 new IpV6Ec2SessionStack(app, "IpV6Ec2SessionStack");
